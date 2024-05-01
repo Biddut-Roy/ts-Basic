c458456163a5b42717d6e5e7816086fcefa7da24
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-01 08:45:34 +0600

    1.4 data type end

---
+++ Typescript/dist/1.4.js
@@ -0,0 +1,7 @@
+"use strict";
+// ts data Type 
+let Author = "Nill roy ts";
+let Money = 123;
+let RW = false;
+let U = undefined;
+let k = null;
---
+++ Typescript/src/1.4.ts
@@ -0,0 +1,7 @@
+// ts data Type 
+
+let Author : string = "Nill roy ts";
+let  Money : number = 123;
+let RW : boolean = false;
+let U : undefined = undefined;
+let k : null = null;
