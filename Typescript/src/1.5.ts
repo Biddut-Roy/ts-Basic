dbb9d1740e5890a249259750dbc8453ac7512f3b
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-01 09:01:41 +0600

    Object declearation in ts

---
+++ Typescript/src/1.5.ts
@@ -0,0 +1,14 @@
+// Object  >> Reference type
+
+const user : {
+    firstName: string;
+    middleName?: string;            //Optional Type
+    LastName: string;
+    // CompanyName: string;
+    CompanyName: 'Start a jack';       // literal type
+} = {
+    firstName: "Nill",
+    middleName: "",
+    LastName: "Roy",
+    CompanyName: "Start a jack"
+}
